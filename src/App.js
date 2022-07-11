16b5d9e2e6446fdfade7f31e63df75aea4e11bcc
adriansunye <adrian.sunye@gmail.com>
2022-07-11 09:59:39 +0200

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Header,About, Footer } from './container';
 import { Navbar } from './components';
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 import './App.css';
 
