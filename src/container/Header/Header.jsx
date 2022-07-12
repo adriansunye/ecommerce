f7b5cad2d2f8070c225996a3f611d9c68eb9962e
ecp12 <106453089+ecp12@users.noreply.github.com>
2022-07-12 11:37:19 +0200

    new

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import './Header.scss';
 
+
 const Header = () => {
   return (
-    <div className="Welcome">
-      <div className="TextGeneral">
-        <h1 className="TextIntro">Empieza tu compra en Dillons</h1>
-        <p className="ParagraphWel">Dillons, tu supermercado de confianza donde encontras de todo y si no tenemos lo que busca, es por que no exite.</p>
-      <div className="Postal">
-        <button class="button2">Entrar</button>
-      </div>
-      </div>
-      
-      <div className="wave">
-        <svg viewBox="0 0 500 150" preserveAspectRatio="none">
-          <path d="M0.00,49.99 C150.00,150.00 349.20,-49.99 500.00,49.99 L500.00,150.00 L0.00,150.00 Z" ></path>
-        </svg>
-    </div>
-    </div>
+    <div>Header</div>
   );
 }
 
--- src/container/index.js
+++ src/container/index.js
@@ -1,5 +1,3 @@
 export { default as Header } from './Header/Header';
-export { default as About } from './About/About';
 export { default as Products } from './Products/Products';
 export { default as Footer } from './Footer/Footer';
-
