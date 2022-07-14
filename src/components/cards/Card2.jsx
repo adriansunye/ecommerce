566075a88ddd9db819d840fc1ba12f56823f8ea1
Eberth <itec_19@hotmail.com>
2022-07-14 14:05:13 +0200

    card2styledone

--- src/components/cards/Card2.jsx
+++ src/components/cards/Card2.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./card2.css";
+import { MdDeleteForever } from "react-icons/md";
+import { GrAdd } from "react-icons/gr";
 
 const Card2 = () => {
     return (
         <div className="G-container">
+
             <div className="container">
                 <div className="cont-img">
                     <img src="./images/garrafa5L.png" alt="" />
@@ -15,13 +18,82 @@ const Card2 = () => {
                 <div className="cont-btn">
                     <h5 className="cont-btn-text">1 unit</h5>
                     <div className="cont-btn-click">
-                        <a href="" className="btn-1">D</a>
-                        <a href="" className="btn-2">+</a>
+                        <a href="" className="btn-1"><MdDeleteForever className="btn-del"/></a>
+                        <a href="" className="btn-2"><GrAdd className="btn-add"/></a>
                     </div>
-
                 </div>
-
             </div>
+
+            <div className="container">
+                <div className="cont-img">
+                    <img src="./images/garrafa5L.png" alt="" />
+                </div>
+                <div className="cont-text">
+                    <h3 className="cont-text-1">The 1982 vintage for Lafite Rothschild</h3>
+                    <h3 className="cont-text-2"><b>E47 EUR*</b></h3>
+                </div>
+                <div className="cont-btn">
+                    <h5 className="cont-btn-text">1 unit</h5>
+                    <div className="cont-btn-click">
+                        <a href="" className="btn-1"><MdDeleteForever className="btn-del"/></a>
+                        <a href="" className="btn-2"><GrAdd className="btn-add"/></a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="cont-img">
+                    <img src="./images/garrafa5L.png" alt="" />
+                </div>
+                <div className="cont-text">
+                    <h3 className="cont-text-1">The 1982 vintage for Lafite Rothschild</h3>
+                    <h3 className="cont-text-2"><b>E47 EUR*</b></h3>
+                </div>
+                <div className="cont-btn">
+                    <h5 className="cont-btn-text">1 unit</h5>
+                    <div className="cont-btn-click">
+                        <a href="" className="btn-1"><MdDeleteForever className="btn-del"/></a>
+                        <a href="" className="btn-2"><GrAdd className="btn-add"/></a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="cont-img">
+                    <img src="./images/garrafa5L.png" alt="" />
+                </div>
+                <div className="cont-text">
+                    <h3 className="cont-text-1">The 1982 vintage for Lafite Rothschild</h3>
+                    <h3 className="cont-text-2"><b>E47 EUR*</b></h3>
+                </div>
+                <div className="cont-btn">
+                    <h5 className="cont-btn-text">1 unit</h5>
+                    <div className="cont-btn-click">
+                        <a href="" className="btn-1"><MdDeleteForever className="btn-del"/></a>
+                        <a href="" className="btn-2"><GrAdd className="btn-add"/></a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="cont-img">
+                    <img src="./images/garrafa5L.png" alt="" />
+                </div>
+                <div className="cont-text">
+                    <h3 className="cont-text-1">The 1982 vintage for Lafite Rothschild</h3>
+                    <h3 className="cont-text-2"><b>E47 EUR*</b></h3>
+                </div>
+                <div className="cont-btn">
+                    <h5 className="cont-btn-text">1 unit</h5>
+                    <div className="cont-btn-click">
+                        <a href="" className="btn-1"><MdDeleteForever className="btn-del"/></a>
+                        <a href="" className="btn-2"><GrAdd className="btn-add"/></a>
+                    </div>
+                </div>
+            </div>
+
+            
+
         </div>
 
     )
