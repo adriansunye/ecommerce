812a7203e2445c0a9da9d9812a13eb7e7379c8b3
Liam Martinez <liammartinez@Liams-Macbook-Pro.local>
2022-07-15 13:51:48 +0200

    guillemDev

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import {Navbar ,Header, Products, Footer } from './container';
 
 
 import React from 'react';
+
+import './App.css';
+
 const App = () => {
   return (
     <div className="app">
       <Navbar />
       <Header />
-      <Products/>
-      <Footer/>
-      
+      <Products />
+      <Footer />
     </div>
-  );
+  )
 }
 
 
