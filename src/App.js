4c1fb63d0de55fcfa5f4267d1ecb8462b5b1ff48
Liam Martinez <liammartinez@Liams-Macbook-Pro.local>
2022-07-15 13:51:48 +0200

    guillemDev

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 
-
 import './App.css';
 import { Header,About, Footer } from './container';
-
 import { Navbar } from './components';
 import React from 'react';
+
+import './App.css';
+
 const App = () => {
   return (
-
     <div className="app">
       <Navbar />
       <Header />
-      <About/>
-      <Footer/>
-       
+      <About />
+      <Footer />
     </div>
-    
-  );
+  )
 }
+
+
+
+
 
 export default App;
