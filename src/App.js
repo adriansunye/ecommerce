98294b8ac6befdb7b14f03ae599f1c5ad730bc7d
ecp12 <106453089+ecp12@users.noreply.github.com>
2022-07-19 10:11:13 +0200

    new

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <Header />
       <About/>
       <Footer/>
-      
+      <Products/>
     </div>
   );
 }
--- src/container/Products/Products.jsx
+++ src/container/Products/Products.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import './Products.scss';
+import react from 'react'
+import axios from 'axios';
+import '../Products/.env'
+const products = () =>{
+    const peticion = axios.get ('https://api.kroger.com/v1/connect/oauth2',{
+      params:{
+      client_id:'comerse-9d90fd2a34db00c9b84e39c0f23d9a924065336273297588816',
+      client_secret:'OZePju4UlZdAIIl9TZZhfBKSCUbfQPEzaIGEqf0i',
+      redirect_uri:'http://localhost:3000',
+      headers: {'Authorization': 'basic '+ process.env.REACT_APP_SECRETTING}
+     }
 
-const Products = () => {
-  return (
-    <div>Products</div>
-  );
+    })
+    console.log(peticion)
+
 }
 
-export default Products;
+export default products;
+
