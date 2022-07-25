200595c2f4540a35ae1dadb7f93d657721a048f4
ecp12 <106453089+ecp12@users.noreply.github.com>
2022-07-25 10:32:44 +0200

    new 2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 
+
 import './App.css';
+import { Header,About, Footer } from './container';
 
-
-import {Navbar ,Header, Products, Footer } from './container';
-
-
+import { Navbar } from './components';
 import React from 'react';
-
-import './App.css';
-
-import './App.css';
-
 const App = () => {
   return (
+
     <div className="app">
       <Navbar />
       <Header />
-      <Products />
-      <Footer />
+      <About/>
+      <Footer/>
+       
     </div>
-  )
+    
+  );
 }
-
-
-
-
 
 export default App;
