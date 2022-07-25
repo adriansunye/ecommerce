0a7b39feba14669ded401f0db4ad3ff97c858861
daliauv1 <dalia.dmuv@gmail.com>
2022-07-25 23:32:44 +0200

    delete duplicate folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/navbar/Navbar';
 import React from 'react';
 import Welcome from './views/welcome/Welcome';
 
-const App = () => {
+function App() {
   return (
 
     <div className="app">
